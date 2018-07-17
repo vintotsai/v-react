53c4078b01fb521edb587e9864896ed2d1a47d5d
vincentotsai <totsai@139.com>
2018-07-17 23:45:19 +0800

    add: 新建功能

--- server/index.js
+++ server/index.js
@@ -7,7 +7,29 @@ app.get('/', function (req, res) {
 })
 
 // 增
+app.get('/flights/add', function (req, res) {
+  res.header({
+    'access-control-allow-origin': '*'
+  })
+  let newFlight = JSON.parse(req.query.values)
+  console.log(newFlight)
+  db.read(function (data) {
+    let list = data.data
+    newFlight.id = Date.now()
+    list.push(newFlight)
+    let newJson = {
+      desc: "从北京至深圳航班信息",
+      data: list
+    }
+    db.write(JSON.stringify(newJson), function (data) {
+      res.json({
+        data:newJson
+      })
+    })
+  })
+})
 
+// 查
 app.get('/flights', function (req, res) {
   res.header({
     'access-control-allow-origin': '*'
--- src/components/flight-list.js
+++ src/components/flight-list.js
@@ -1,6 +1,65 @@
 import React from 'react'
-import { Table, Popconfirm } from 'antd'
+import { Table, Popconfirm, Button, Modal, Form, Input, Radio} from 'antd'
 import axios from 'axios'
+
+const FormItem = Form.Item
+
+const FlightCreateForm = Form.create()(
+  class extends React.Component {
+    render() {
+      const { visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      return (
+        <Modal
+          visible={visible}
+          title="新建航班信息"
+          okText="新建"
+          cancelText="取消"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form layout="vertical">
+            <FormItem label="航班号">
+              {getFieldDecorator('flightNo', {
+                rules: [{ required: true, message: '请输入航班号!' }],
+              })(
+                <Input placeholder="深圳航空ZH9112"/>
+              )}
+            </FormItem>
+            <FormItem label="起飞时间">
+              {getFieldDecorator('departureTime', {
+                rules: [{ required: true, message: '请输入起飞时间!' }],
+              })(
+                <Input placeholder="00:00"/>
+              )}
+            </FormItem>
+            <FormItem label="降落时间">
+              {getFieldDecorator('arrivalTime', {
+                rules: [{ required: true, message: '请输入降落时间!' }],
+              })(
+                <Input placeholder="00:00"/>
+              )}
+            </FormItem>
+            <FormItem label="机票价格">
+              {getFieldDecorator('price', {
+                rules: [{ required: true, message: '请输入价格!' }],
+              })(
+                <Input placeholder="1920"/>
+              )}
+            </FormItem>
+            <FormItem label="折扣">
+              {getFieldDecorator('discount', {
+                rules: [{ required: true, message: '请输入折扣!' }],
+              })(
+                <Input placeholder="6.5"/>
+              )}
+            </FormItem>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+)
 
 export default class FlightList extends React.Component {
   constructor() {
@@ -8,7 +67,8 @@ export default class FlightList extends React.Component {
     this.state = {
       list: [],
       flightDesc: '',
-      sysTime: Date.now()
+      sysTime: Date.now(),
+      visible:false
     }
     this.columns = [{
       title: '航班号',
@@ -95,14 +155,52 @@ export default class FlightList extends React.Component {
       console.log(res.msg)
     })
   }
+  handleCancel = () => {
+    this.setState({ visible: false })
+  }
+  handleCreate = () => {
+    const form = this.formRef.props.form
+    form.validateFields((err, values) => {
+      if (err) {
+        return false
+      }
 
+      console.log('Received values of form>>>', values)
+      axios.get('http://localhost:1234/flights/add',{params: { values}}).then(res=>{
+        console.log(res)
+        let data = res.data.data.data
+        data.map((item, index) => { item.key = item.id })
+
+        this.setState({
+          list: data
+        })
+      })
+      form.resetFields()
+      this.setState({ visible: false })
+
+    });
+  }
+  saveFormRef = (formRef) => {
+    this.formRef = formRef;
+  }
+  showModal = () => {
+    this.setState({visible: true})
+  }
   render() {
     return (
       <div>
         <p className="App-intro" onClick={() => this.setState({ flightDesc: 'Missing..' })}>{this.state.flightDesc}</p>
         {/* <p>{this.state.sysTime}</p> */}
         {/* <p>{this.props.tot}</p> */}
-
+        <div className="list-btn">
+          <Button type="primary" onClick={this.showModal}>新建</Button>
+        </div> 
+        <FlightCreateForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
         <Table columns={this.columns} dataSource={this.state.list} onChange={this.onChange}></Table>
       </div>
     )
